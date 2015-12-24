924aff58b9ff9bf392f2b973e7907da8d582dd01
Sneha S <snehasomwanshi@gmail.com>
2015-12-24 14:03:57 +0530

    Print issues in table format

--- lib/Issues/cli.ex
+++ lib/Issues/cli.ex
@@ -27,7 +27,14 @@ defmodule Issues.CLI do
 
   def process({user, project, count}) do
     Issues.GitHubIssues.fetch(user, project) |>
-    Enum.sort( fn(x, y) -> x["created_at"] <= y["created_at"] end)
+    Enum.sort( fn(x, y) -> x["created_at"] <= y["created_at"] end) |>
+    print_table
+  end
+
+  def print_table(dict) do
+    IO.puts "Number  | Title | Create At"
+    for d <- dict,
+     do: IO.puts "#{d["number"]} | #{d["title"]}  | #{d["created_at"]}"
   end
 
 end
--- lib/Issues/git_hub_issues.ex
+++ lib/Issues/git_hub_issues.ex
@@ -2,10 +2,9 @@ defmodule Issues.GitHubIssues do
   @user_agent  [{ "User-agent", "Elixir snehasomwanshi@gmail.com"}]
 
   def fetch(user, project) do
-    IO.inspect "https://api.github.com/repos/#{user}/#{project}/issues" |>
+    "https://api.github.com/repos/#{user}/#{project}/issues" |>
     HTTPoison.get(@user_agent) |>
     handle_response
-
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
